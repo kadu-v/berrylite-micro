b8fbc5c14fbcf44fd0034fff086073a295d4d448
kadu-v <kazuonsunday@yahoo.co.jp>
2023-06-18 01:01:47 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 use flatbuffers;
 use verdigiris::tflite_schema_generated::tflite;
 const BUFFER: &[u8; 3164] = include_bytes!("../models/hello_world_float.tflite");
+// const BUFFER_PERSON: &[u8; 300568] = include_bytes!("../models/person_detect.tflite");
 
 fn main() {
     let model = tflite::root_as_model(BUFFER).unwrap();
+    let table = model.description();
+    println!("{:?}", table);
+    let x = model.operator_codes();
+    println!("{:?}", x);
 
-    let metadata = model.metadata().unwrap();
+    let y = model.subgraphs().unwrap();
+    println!("{}", y.len());
 
-    for e in metadata {
-        println!("{:?}", e);
+    let layer = y.get(0);
+    let operators = layer.operators().unwrap();
+
+    for op in operators {
+        println!("{:?}", op);
+    }
+
+    let tensors = layer.tensors().unwrap();
+    for tensor in tensors {
+        println!("{:?}", tensor);
     }
 }
