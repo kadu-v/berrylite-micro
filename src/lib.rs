505387669d04c92312928888d5974ce68620f047
kadu-v <kazuonsunday@yahoo.co.jp>
2023-06-17 23:57:08 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 pub mod kernels;
+
+pub mod tflite_schema_generated;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use flatbuffers;
+use verdigiris::tflite_schema_generated::tflite;
+const BUFFER: &[u8; 3164] = include_bytes!("../models/hello_world_float.tflite");
+
 fn main() {
-    println!("Hello, world!");
+    let model = tflite::root_as_model(BUFFER).unwrap();
+
+    let metadata = model.metadata().unwrap();
+
+    for e in metadata {
+        println!("{:?}", e);
+    }
 }
