49f69627ab34ddfae54c7f0846c804e405c934ba
kadu-v <kazuonsunday@yahoo.co.jp>
2023-06-24 19:17:31 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::mem::size_of;
+
 use berrylite::tflite_schema_generated::tflite;
 use flatbuffers;
 const BUFFER: &[u8; 3164] = include_bytes!("../models/hello_world_float.tflite");
@@ -19,20 +21,34 @@ fn main() {
     let tensor = tensors.get(0);
     println!("{:?}", tensor);
 
-    let buffer = buffers.get(0);
+    let buffer = buffers.get(2);
     println!("{:?}", buffer);
 
-    for (i, tensor) in tensors.iter().enumerate() {
-        let buffer = buffers.get(tensor.buffer() as usize);
-        println!("{}", i);
-        println!("{:?}", tensor);
-        println!(
-            "{}: {:?}\n",
-            buffer.data().unwrap_or_default().len(),
-            buffer
-        );
-    }
+    let data = buffer.data().unwrap();
+    println!("data: {:?}", data);
 
-    let op = subgraph.operators().unwrap().get(0);
-    println!("{:?}", op);
+    let row_data = data.bytes();
+    println!("{:?}, len: {}", row_data, row_data.len());
+
+    unsafe {
+        let v = core::slice::from_raw_parts(
+            row_data.as_ptr() as *const f32,
+            row_data.len() / size_of::<f32>(),
+        );
+        println!("{:?}", v);
+        println!("{}", v.len());
+    }
+    // for (i, tensor) in tensors.iter().enumerate() {
+    //     let buffer = buffers.get(tensor.buffer() as usize);
+    //     println!("{}", i);
+    //     println!("tensor: {:?}", tensor);
+    //     println!(
+    //         "{}: {:?}\n",
+    //         buffer.data().unwrap_or_default().len(),
+    //         buffer
+    //     );
+    // }
+
+    // let op = subgraph.operators().unwrap().get(0);
+    // println!("{:?}", op);
 }
