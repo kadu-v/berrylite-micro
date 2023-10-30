6acdd0fb719fb4fd6439d9398d42e17e16d1b970
kadu-v <kazuonsunday@yahoo.co.jp>
2023-10-30 21:49:02 +0900

    Add: test case of fully_connected for float

--- src/kernel/micro_operator/f32/test_fully_connected.rs
+++ src/kernel/micro_operator/f32/test_fully_connected.rs
@@ -1,7 +1,7 @@
 use super::fully_connected::OpFullyConnected;
 use crate::kernel::utils::{testing::Tensor, types::flat_skip_dims};
 
-fn mk_testcase(input: Tensor<f32>, filter: Tensor<f32>, expected: Tensor<f32>) {
+fn mk_test_case(input: Tensor<f32>, filter: Tensor<f32>, expected: Tensor<f32>) {
     assert_eq!(
         input.shape.len(),
         2,
@@ -25,7 +25,7 @@ fn mk_testcase(input: Tensor<f32>, filter: Tensor<f32>, expected: Tensor<f32>) {
     let output_data_size = output_shape.iter().fold(1, |x, acc| x * acc) as usize;
     let mut output_data = vec![0f32; output_data_size];
 
-    let batches = flat_skip_dims(&output_shape, output_shape.len() - 1);
+    let batches = flat_skip_dims(&output_shape[..], output_shape.len() - 1);
     let output_depth = filter.shape[filter.shape.len() - 2] as i32;
     let accum_depth = filter.shape[filter.shape.len() - 1] as i32;
 
@@ -53,23 +53,44 @@ fn test_fully_connected_2x3_3x2() {
 
     let input = Tensor::ones(&input_shape);
     let filter = Tensor::ones(&filter_shape);
-    let expected = Tensor::from(vec![2., 2., 2., 2., 2., 2., 2., 2., 2.], vec![3, 3]);
-    mk_testcase(input, filter, expected);
+
+    #[rustfmt::skip]
+    let expected = Tensor::from(
+        vec![
+            2., 2., 2., 
+            2., 2., 2., 
+            2., 2., 2.], 
+        vec![3, 3] /* expected tensor shape */);
+    mk_test_case(input, filter, expected);
 }
 
 #[test]
 fn test_fully_connected_3x3_3x4() {
-    let filter_shape = [3, 3];
-    let input_shape = [3, 4];
-
+    #[rustfmt::skip]
     let filter = Tensor::from(
-        vec![0.0, 0.1, 0.2, 1.0, 1.1, 1.2, 2.0, 2.1, 2.2],
-        vec![3, 3],
+        vec![
+            0.0, 0.1, 0.2, 
+            1.0, 1.1, 1.2, 
+            2.0, 2.1, 2.2],
+        vec![3, 3] /* filter shape */,
     );
+    #[rustfmt::skip]
     let input = Tensor::from(
-        vec![0.0, 0.2, 0.4, 0.6, 1.0, 1.2, 1.4, 1.6, 2.0, 2.2, 2.4, 2.6],
-        vec![3, 4],
+        vec![
+            0.0, 0.2, 0.4, 
+            0.6, 1.0, 1.2, 
+            1.4, 1.6, 2.0, 
+            2.2, 2.4, 2.6],
+        vec![3, 4] /* input shape */,
     );
-    let expected = Tensor::from(vec![2., 2., 2., 2., 2., 2., 2., 2., 2.], vec![3, 3]);
-    mk_testcase(input, filter, expected);
+    #[rustfmt::skip]
+    let expected = Tensor::from(
+        vec![
+            0.10000001, 0.70000005, 1.3000001, 
+            0.34,       3.14,       5.94, 
+            0.56,       5.5600004,  10.559999,
+        ],
+        vec![3, 3] /* expected tensor shape */,
+    );
+    mk_test_case(input, filter, expected);
 }
--- src/kernel/utils/testing.rs
+++ src/kernel/utils/testing.rs
@@ -7,29 +7,29 @@ where
     T: ArrayElem<T>,
 {
     pub data: Vec<T>,
-    pub shape: Vec<usize>,
+    pub shape: Vec<i32>,
 }
 
 impl<T> Tensor<T>
 where
     T: ArrayElem<T>,
 {
-    pub fn from(data: Vec<T>, shape: Vec<usize>) -> Self {
+    pub fn from(data: Vec<T>, shape: Vec<i32>) -> Self {
         Self { data, shape }
     }
 
-    pub fn ones(shape: &[usize]) -> Self {
+    pub fn ones(shape: &[i32]) -> Self {
         assert!(shape.len() > 0, "expected non empty, but got 0");
-        let tot = shape.iter().fold(1usize, |x, &acc| x * acc);
+        let tot = shape.iter().fold(1usize, |x, &acc| x * acc as usize);
         let v = FromPrimitive::from_usize(1).unwrap();
         let data = vec![v; tot];
         let shape = shape.iter().cloned().collect();
         Self { data, shape }
     }
 
-    pub fn zeros(shape: &[usize]) -> Self {
+    pub fn zeros(shape: &[i32]) -> Self {
         assert!(shape.len() > 0, "expected non empty, but got 0");
-        let tot = shape.iter().fold(1, |x, &acc| x * acc);
+        let tot = shape.iter().fold(1, |x, &acc| x * acc as usize);
         let v = FromPrimitive::from_usize(0).unwrap();
         let data = vec![v; tot];
         let shape = shape.iter().cloned().collect();
