ca54bdc91b1b4622be4a7b8800fbc23e43b35158
kadu-v <kazuonsunday@yahoo.co.jp>
2023-11-14 00:20:22 +0900

    Add: integration test for person_detection model

--- tests/person_detection_int8.rs
+++ tests/person_detection_int8.rs
@@ -1,7 +1,9 @@
-use berrylite::kernel::micro_operator::f32::{
-    conv2d::OpConv2D, fully_connected::OpFullyConnected, max_pool2d::OpMaxPool2D,
-    reshape::OpReshape, softmax::OpSoftMax,
-};
+use berrylite::kernel::micro_operator::i8::avg_pool2d_i8::OpAvgPool2DInt8;
+use berrylite::kernel::micro_operator::i8::conv2d_i8::OpConv2DInt8;
+use berrylite::kernel::micro_operator::i8::depthwise_conv2d_i8::OpDepthWiseConv2DInt8;
+use berrylite::kernel::micro_operator::i8::fully_connected_i8::OpFullyConnectedInt8;
+use berrylite::kernel::micro_operator::i8::reshape_i8::OpReshapeInt8;
+use berrylite::kernel::micro_operator::i8::softmax_i8::OpSoftMaxInt8;
 use berrylite::micro_allocator::BumpArenaAllocator;
 use berrylite::micro_errors::Result;
 use berrylite::micro_interpreter::BLiteInterpreter;
@@ -11,56 +13,63 @@ use image::{ImageBuffer, Luma};
 
 const BUFFER: &[u8; 300568] = include_bytes!("../resources/models/person_detect.tflite");
 
-const ARENA_SIZE: usize = 160 * 1024;
+const ARENA_SIZE: usize = 1024 * 1024;
 static mut ARENA: [u8; ARENA_SIZE] = [0; ARENA_SIZE];
 
 fn set_input(
-    interpreter: &mut BLiteInterpreter<'_, f32>,
+    interpreter: &mut BLiteInterpreter<'_, i8>,
     input_h: usize,
     input_w: usize,
-    image: &Vec<u8>,
+    _input_zero_point: i32,
+    image: &[u8],
 ) {
     for h in 0..input_h {
         for w in 0..input_w {
-            interpreter.input.data[h * input_w + w] = image[h * input_w + w] as f32 / 255.;
+            let v = image[h * input_w + w];
+            interpreter.input.data[h * input_w + w] = v as i8;
         }
     }
 }
 
-fn predict(input: &Vec<u8>) -> Result<usize> {
+fn predict(image: &Vec<u8>) -> Result<usize> {
     let model = tflite::root_as_model(BUFFER).unwrap();
 
     let mut allocator = unsafe { BumpArenaAllocator::new(&mut ARENA) };
 
-    let mut op_resolver = BLiteOpResolver::<5, f32, _>::new();
-    op_resolver.add_op(OpFullyConnected::fully_connected())?;
-    op_resolver.add_op(OpReshape::reshape())?;
-    op_resolver.add_op(OpConv2D::conv2d())?;
-    op_resolver.add_op(OpMaxPool2D::max_pool2d())?;
-    op_resolver.add_op(OpSoftMax::softmax())?;
+    let mut op_resolver = BLiteOpResolver::<7, _, _>::new();
+    op_resolver.add_op(OpFullyConnectedInt8::fully_connected_int8())?;
+    op_resolver.add_op(OpReshapeInt8::reshape_int8())?;
+    op_resolver.add_op(OpConv2DInt8::conv2d_int8())?;
+    op_resolver.add_op(OpAvgPool2DInt8::avg_pool2d_int8())?;
+    op_resolver.add_op(OpSoftMaxInt8::softmax_int8())?;
+    op_resolver.add_op(OpDepthWiseConv2DInt8::depthwise_conv2d_int8())?;
 
     let mut interpreter = BLiteInterpreter::new(&mut allocator, &op_resolver, &model)?;
+    let (_input_scale, input_zero_point) = interpreter.get_input_quantization_params().unwrap();
+    let (output_scale, output_zero_point) = interpreter.get_output_quantization_params().unwrap();
 
-    set_input(&mut interpreter, 28, 28, input);
+    set_input(&mut interpreter, 96, 96, input_zero_point, image);
     interpreter.invoke()?;
 
     let output = interpreter.output;
+    dbg!(output);
     let mut num_prob = 0.;
     let mut num = 0;
-    for (i, &prob) in output.data.iter().enumerate() {
+    for (i, &y_pred) in output.data.iter().enumerate() {
+        let prob = output_scale * (y_pred as i32 - output_zero_point) as f32;
+        dbg!(prob);
         if prob > num_prob {
             num_prob = prob;
             num = i;
         }
     }
-    dbg!(&output.data);
     Ok(num)
 }
 
 fn make_vec_from_image(img: &ImageBuffer<Luma<u8>, Vec<u8>>) -> Vec<u8> {
     let mut v = Vec::new();
-    for h in 0..28 {
-        for w in 0..28 {
+    for h in 0..96 {
+        for w in 0..96 {
             let p = img.get_pixel(w, h).0[0];
             v.push(p);
         }
@@ -70,17 +79,24 @@ fn make_vec_from_image(img: &ImageBuffer<Luma<u8>, Vec<u8>>) -> Vec<u8> {
 
 #[test]
 fn test_person_detection_int8() {
-    let inputs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let inputs = [
+        ("person0", 0),
+        ("person1", 0),
+        ("mario0", 0),
+        ("dog0", 1),
+        ("cat0", 1),
+    ];
 
-    for expected in inputs {
-        let img_path = format!("./resources/dataset/mnist/{}.jpg", expected);
-        let img = image::open(&img_path).unwrap().into_luma8();
+    for (img_name, expected) in inputs {
+        let img_path = format!("./resources/dataset/person_detection/{}.jpg", img_name);
+        let img = image::open(&img_path).unwrap();
+        dbg!(img.height(), img.width());
+        let img = img.into_luma8();
         let input = make_vec_from_image(&img);
         let y_pred = match predict(&input) {
             Ok(y_pred) => y_pred,
             Err(e) => {
-                println!("Error: {:?}", e);
-                return;
+                panic!("Error: {:?}", e);
             }
         };
         assert_eq!(
