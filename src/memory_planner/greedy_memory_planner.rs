208c0abbc7a0105b8046db56c9c392a6a2a5de2c
kadu-v <kazuonsunday@yahoo.co.jp>
2023-11-26 23:46:01 +0900

    wip

--- src/memory_planner/greedy_memory_planner.rs
+++ src/memory_planner/greedy_memory_planner.rs
@@ -1,3 +1,26 @@
+use crate::micro_allocation_info::AllocationInfo;
+/*-----------------------------------------------------------------------------*/
+/* Struct for a List Entry                                                     */
+/*-----------------------------------------------------------------------------*/
+
+/*-----------------------------------------------------------------------------*/
+/* Struct for a GreedyMemoryPlanner                                            */
 /*-----------------------------------------------------------------------------*/
 #[derive(Debug)]
 pub struct GreedyMemoryPlanner {}
+
+impl GreedyMemoryPlanner {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn create_plan(&self, allocation_info: &mut [AllocationInfo]) {}
+
+    fn insert() {}
+
+    fn does_entry_overlap_in_time() {}
+
+    fn next_simultaneous_active_buffer() {}
+
+    fn calculate_offsets_if_needed() {}
+}
